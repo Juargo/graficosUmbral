bf16bc8b369c74935cc5575a1a30b6038a4f9604
juargo <jorgeretamalaburto@gmail.com>
2016-12-15 18:38:28 -0300

    diastabfuncional

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ app.listen(3000, function () {
 
 app.get('/api/getdata', function (req, res) {
     var target = req.query.target;
+    var fi = req.query.fi
+    var ff = req.query.ff
     target = target.replace(/ /g, '%20').replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/,/g, '%2C').replace(/\"/g, '%22');
 
-    var from = "00:00_20161205";
-    var until = "00:00_20161206";
+    var from = "00:00_201612" + fi;
+    var until = "00:00_201612" + ff;
     var tz = "America/Santiago";
     var format = "json";
 
